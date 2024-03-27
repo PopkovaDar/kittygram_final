20036a440f3e91b49a6e71b0e3566e7c9ee0099b
Попкова Дарья <daryapopkova90@yandex.ru>
2024-03-27 15:27:41 +0300

    on review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from django.core.management.utils import get_random_secret_key
 
 
 load_dotenv()
